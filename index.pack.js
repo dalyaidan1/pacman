996a05bc734999eead66a9a092fd446797184923
Aidan Daly <dalyaidan1@gmail.com>
2021-04-05 21:05:03 -0400

    added left/right shortcut

--- index.pack.js
+++ index.pack.js
@@ -103,6 +103,9 @@ function control(e) {
             pacmanCurrentIndex % width !==0
             ) 
             pacmanCurrentIndex -=1
+            if (pacmanCurrentIndex === 364) {
+                pacmanCurrentIndex = 391
+            }
         break
         case 39:
         console.log('pressed right')
@@ -112,6 +115,9 @@ function control(e) {
             pacmanCurrentIndex % width < width -1
             ) 
             pacmanCurrentIndex +=1
+            if (pacmanCurrentIndex === 391) {
+                pacmanCurrentIndex = 364
+            }
         break
     }
     squares[pacmanCurrentIndex].classList.add('pacman')
